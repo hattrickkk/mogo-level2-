f020bd5ed79ce6d67b7bec76ff1f0431d871a750
hattrick <dasha.khitrick@gmail.com>
2023-01-09 18:49:11 +0300

    add almost everything

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector('input[type=button]').onclick = function() {
-		console.log(new Date())
-	}
-})
+
+$(document).ready(function () {
+	$('.accordion__top').click(function (event) {
+		$(this).toggleClass('active').next().slideToggle(300);
+	});
+});
+
+const swiper = new Swiper('.reviews__slider', {
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	centeredSlides: true,
+	initialSlide: 1,
+	autoplay: true,
+	speed: 500,
+	autoHeight: true,
+});
